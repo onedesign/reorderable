b161c3197511953abfd928f66f4599c246ac0176
AJ Schuster <ajs@maximumaardvark.com>
2012-10-10 13:50:58 -0400

    Get the helper working with Rails 3.1+

--- app/helpers/reorderable_helper.rb
+++ app/helpers/reorderable_helper.rb
@@ -1,5 +1,5 @@
 module ReorderableHelper
   def reorderable_attributes(collection, reorder_column='position')
-    " class='reorderable' data-reorder-url='#{url_for(:controller => "admin/#{collection.first.class.to_s.tableize}", :action => 'reorder', :only_path => true)}' data-reorder-column='#{reorder_column}'"
+    %Q[ class="reorderable" data-reorder-url="#{url_for(:controller => "admin/#{collection.first.class.to_s.tableize}", :action => 'reorder', :only_path => true)}" data-reorder-column="#{reorder_column}"].html_safe
   end
 end
